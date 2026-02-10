1c7c3c5d7518e11c71f13fbf5ec989d737dbc1b1
sweenu <contact@sweenu.xyz>
2026-02-10 22:25:06 +0100

    Scaffold PocketBase Go project

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// Custom routes will be registered here
+		return se.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ tools.go
@@ -0,0 +1,8 @@
+//go:build tools
+
+package main
+
+import (
+	_ "github.com/fxamacker/cbor/v2"
+	_ "github.com/veraison/go-cose"
+)
